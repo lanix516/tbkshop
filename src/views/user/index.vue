b6a4f005fb2cf9b4e1e830e20ae12dba288df707
张驰 <82183509@qq.com>
2019-04-01 14:42:37 +0800

    细节修改

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -31,7 +31,7 @@
       </van-cell>
     </van-cell-group>
     <van-cell-group title="返利操作" class="user-group">
-      <van-cell icon="cash-back-record" title="提取返利" is-link url="/#/cash"/>
+      <van-cell icon="cash-back-record" title="提取返利" is-link url="/cash"/>
       <van-cell icon="records" title="提取记录" is-link/>
       <van-cell
         class="push-btn"
@@ -46,7 +46,7 @@
 
     <van-cell-group title="账户">
       <van-cell icon="shopping-cart-o" title="全部订单" is-link/>
-      <van-cell icon="alipay" title="返利账号" is-link url="/#/alipay"/>
+      <van-cell icon="alipay" title="返利账号" is-link url="/alipay"/>
     </van-cell-group>
     <van-popup v-model="showLeaveMessage">
       <van-cell-group>
